49a40f4facb73d8647232ae4e024095b45f3da45
Hary369 <122548103+Hary369@users.noreply.github.com>
2024-11-15 23:15:38 +0100

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+let nom = [];
+let email = [];
+
+let btn = document.querySelector("button");
+
+btn.onclick = AjoutPart(nom, email);
+
+function AjoutPart(nom, email) {
+  nom.push(document.getElementById("nm").value);
+  email.push(document.getElementById("em").value);
+  for (let i = 0; i < nom.lenght; i++) {
+    document.getElementById("p").innerHTML += nom[i] + email[i];
+  }
+}
