092905c05494c491bfdb461e252797bb9d51ca30
Hary369 <122548103+Hary369@users.noreply.github.com>
2024-12-07 16:00:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,10 +27,8 @@ function envoyerMail(email) {
     to: email,
     subject: "1 seul cadeau de Noël par personne",
     text: "Noël",
-    html:
-      "<p>Oyez Oyez bonnes gens !</br> Cette année, le Père Noël prévoit son arrivée à Andernos dans la nuit du 21 au 22 décembre !!!! </br> Qu'on se le dise, qu'on se le dise !!!! </br> Afin de lui venir en aide, le petit lutin Gaëtan a crée un logiciel de tirage aléatoire de binômes. Le sort te désigne pour offrir un cadeau à </p>" +
-      "<p>Chacun pourra arriver et repartir à la date qui lui convient le mieux. </br> D'ici là, je te souhaite une belle fin d'automne et si tu es allergique ou que tu n'aimes pas un aliment, n'hésite pas à me le dire ! </br> Bisous bisous </br> Mireille</p>",
-  };
+    html: "argh",
+      };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
